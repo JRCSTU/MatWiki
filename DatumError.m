3633dd9a9ef53bce029db289dcdd8177618b1b98
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-02-14 18:31:52 +0100

    datumerr:better tip, docs

--- DatumError.m
+++ DatumError.m
@@ -1,11 +1,7 @@
 classdef DatumError < MException
-% An exception that curres an extra "datum" related to the error.
+% An exception that keeps a "datum" related to the cause of the error.
 %
-% Use the static method below to throw this exception::
-%
-%       DatumError.error(theDatum, 'some:identifier', 'Description for %s", something);
-%
-% And contrary to `MException()`, the var-args to the format-string can be many types.
+% Contrary to `MException()`, the var-args to the format-string can be many types.
     
     properties
         Datum
@@ -16,7 +12,7 @@ classdef DatumError < MException
         % It uses `sprintf` internally bc MException() constructor accepts only string & scalars.
         
             obj@MException(identifier, ...
-                '%s\n\nTIP: to retrieve the `%s` datum saved on the error, use:\n    DatumError.last.Datum', ...
+                '%s\n\nTIP: there is a `%s` related to this error:\n    DatumError.last.Datum', ...
                 sprintf(message, varargin{:}), class(datum));
             obj.Datum = datum;
         end
